903ccc1f5e5c2c4cdb385b9e25cd65ea59e01fd9
Junbo Zhang <zhangjunbo1@xiaomi.com>
2022-11-15 05:21:33 +0800

    This will definitely break in 2052 (TODO)

--- deploy/example-app.cc
+++ deploy/example-app.cc
@@ -1,66 +1,39 @@
 #include <cstdio>
-#include <cstdlib>
-#include <vector>
-#include <torch/script.h>
+#include "yesno.h"
 
-std::vector<float> load_audio(const char *audio_path, bool offset_44 = true) {
-  FILE *fp = fopen(audio_path, "r");
+const int MAX_BUF_SIZE = 128 * 1024;
+
+int read_file(const char *file, char *buf, bool offset_44 = true) {
+  FILE *fp = fopen(file, "r");
   int offset = offset_44 ? 44 : 0;
   fseek(fp, 0, SEEK_END);
-  int size = ftell(fp) - offset;
-  char *buf = (char *)malloc(size);
+  int bytes_num = ftell(fp) - offset;
+  if (bytes_num > MAX_BUF_SIZE) return 0;
   fseek(fp, offset, SEEK_SET);
-  fread(buf, sizeof(char), size, fp);
+  bytes_num = fread(buf, sizeof(char), bytes_num, fp);
   fclose(fp);
-
-  std::vector<float> waveform;
-  for (int i = 0; i < size; i += 2) {
-    waveform.push_back(*(short *)(buf + i) / 32768.0);
-  }
-
-  free(buf);
-  return waveform;
+  return bytes_num;
 }
 
 int main(int argc, const char *argv[]) {
   if (argc != 3) {
-    std::cerr << "usage: example-app <model-path> <wav-path>\n";
+    puts("usage: example-app <model-path> <wav-path>");
     return -1;
   }
-
   const char *model_path = argv[1];
   const char *wav_path = argv[2];
 
-  torch::jit::script::Module model;
-  try {
-    model = torch::jit::load(model_path);
-  } catch (const c10::Error &e) {
-    std::cerr << "error loading the model\n";
-    return -1;
-  }
+  // load audio
+  char wav_buf[MAX_BUF_SIZE];
+  int wav_size = read_file(wav_path, wav_buf);
+  auto waveform = convert_audio_from_int16_to_fp32(wav_buf, wav_size);
 
-  // Create a vector of inputs.
-  auto waveform = load_audio(wav_path);
+  // load model
+  void *model = load_model(model_path);
 
-  std::vector<torch::jit::IValue> inputs;
-  inputs.push_back(torch::from_blob(waveform.data(), {1, 1, waveform.size()}));
+  // recognize
+  std::string result = recognize(model, waveform);
+  puts(result.c_str());
 
-  // Execute the model and turn its output into a tensor.
-  auto logits = model.forward(inputs);
-
-  // Greedy search
-  auto align = at::squeeze(at::argmax(logits.toTensor(), 2), 1);
-  std::vector<long> align_v(align.data_ptr<long>(), align.data_ptr<long>() + align.size(0));
-
-  // The final result
-  std::stringstream result_stream;
-  char index_to_char[] = "eYESNO ";
-  for (size_t i = 0; i < align_v.size(); i++) {
-    if (align_v[i] != 0 && (i == 0 || align_v[i] != align_v[i - 1])) {
-      result_stream << index_to_char[align_v[i]];
-    }
-  }
-  auto result = result_stream.str();
-
-  std::cout << result << std::endl;
+  delete_model(model);
 }
